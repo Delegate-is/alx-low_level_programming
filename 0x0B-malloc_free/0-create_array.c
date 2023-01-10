c5a8d3d7dbabfcbe14c3394e9b2aa6f448c24f2c
Delegat-is <kelvinjuma903@gmail.com>
2023-01-10 11:35:18 -0800

    0. Float like a butterfly, sting like a bee

--- 0x0B-malloc_free/0-create_array.c
+++ 0x0B-malloc_free/0-create_array.c
@@ -2,8 +2,9 @@
 #include <stdlib.h>
 
 /**
- * *create_array - creates an array of chars, and initializes it with a specific char
- * 
+ * *create_array - creates an array of chars and initializes it with 
+ * a specific char
+ *
  * @size: size of the array
  * @c: the character to initialize the array with
  *
