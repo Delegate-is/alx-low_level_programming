cfecd8b0acdaff3694fe694dbb0cccec2d8314e4
Delegat-is <kelvinjuma903@gmail.com>
2023-01-17 06:33:39 -0800

    0. Poppy

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -1,13 +1,11 @@
-#define _DOG_H_
 #ifndef _DOG_H_
-
+#define _DOG_H_
 /**
- * struct dog - describes a sweet boy or girl
- * @name - name of the dog
- * @age - how old is the dog?
- * @owner - who is lucky to have the dog
-*/
-
+ * struct dog - describes the sweet boy or girl
+ * @name: name of dog
+ * @age: how old is that dog?
+ * @owner: who is the lucky person that gets to live with that dog?
+ */
 struct dog
 {
 	char *name;
