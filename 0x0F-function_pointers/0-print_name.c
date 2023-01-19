9b119d8024b0b545e86e60627cc8a03c77321412
Delegat-is <kelvinjuma903@gmail.com>
2023-01-19 13:03:32 -0800

    0. What's my name

--- 0x0F-function_pointers/0-print_name.c
+++ 0x0F-function_pointers/0-print_name.c
@@ -1,13 +1,10 @@
 #include "function_pointers.h"
-
 /**
- * void print_name - prints a name
- * @name: name to be printed
+ * print_name - prints a name
+ * @name: the name to print
  * @f: the function to send the name to
  * Return: nothing
- *
-*/
-
+ */
 void print_name(char *name, void (*f)(char *))
 {
 	if (!name || !f)
